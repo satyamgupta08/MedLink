eef65b050a2b8af9656d4cc328239bc825b9032b
Piyush Kumar <piyushkumar07999@gmail.com>
2024-05-17 14:25:26 +0530

    commmiting changes

--- src/components/PatientList.js
+++ src/components/PatientList.js
@@ -1,18 +1,38 @@
 import React from "react";
-import ListGroup from "react-bootstrap/ListGroup";
-import "../styleElements/PatientList.css"; 
 
 const PatientList = ({ props }) => {
-  const { fullName, email, phone, date, time,} = props;
+  const { fullName, email, phone, date, time,message} = props;
 
   return (
-    <ListGroup className="patient-list"> {/* Apply the custom class */}
-      <ListGroup.Item className="list-item">Name: {fullName}</ListGroup.Item> {/* Apply custom class to items */}
-      <ListGroup.Item className="list-item">Email: {email}</ListGroup.Item>
-      <ListGroup.Item className="list-item">Phone: {phone}</ListGroup.Item>
-      <ListGroup.Item className="list-item">Date: {date}</ListGroup.Item>
-      <ListGroup.Item className="list-item">Time: {time}</ListGroup.Item>
-    </ListGroup>
+    <div className="border border-gray-300 rounded-lg shadow-md p-6 bg-white hover:shadow-lg transition duration-300">
+      <div className="text-gray-800 font-semibold text-lg mb-4">Patient Details</div>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex items-center space-x-2 text-gray-700 border-b border-gray-200 pb-2">
+          <span className="font-semibold w-24">Name:</span>
+          <span>{fullName}</span>
+        </div>
+        <div className="flex items-center space-x-2 text-gray-700 border-b border-gray-200 pb-2">
+          <span className="font-semibold w-24">Email:</span>
+          <span>{email}</span>
+        </div>
+        <div className="flex items-center space-x-2 text-gray-700 border-b border-gray-200 pb-2">
+          <span className="font-semibold w-24">Phone:</span>
+          <span>{phone}</span>
+        </div>
+        <div className="flex items-center space-x-2 text-gray-700 border-b border-gray-200 pb-2">
+          <span className="font-semibold w-24">Date:</span>
+          <span>{date}</span>
+        </div>
+        <div className="flex items-center space-x-2 text-gray-700 border-b border-gray-200 pb-2">
+          <span className="font-semibold w-24">Time:</span>
+          <span>{time}</span>
+        </div>
+        <div className="flex items-center space-x-2 text-gray-700 border-b border-gray-200 pb-2">
+          <span className="font-semibold w-24"> Message: </span>
+          <span>{message}</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
