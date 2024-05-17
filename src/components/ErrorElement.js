b8afe3ab2332b3c5dfb4e1b3d0aa455e77174165
Piyush Kumar <piyushkumar07999@gmail.com>
2024-05-17 15:00:50 +0530

    commmiting changes

--- src/components/ErrorElement.js
+++ src/components/ErrorElement.js
@@ -2,34 +2,12 @@ import React from "react";
 
 const ErrorElement = () => {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>Oops! Something went wrong</h1>
-      <p style={styles.message}>We encountered an error while processing your request.</p>
-      {/* <img src="path/to/error-image.png" alt="Error" style={styles.image} /> */}
+    <div className="text-center p-4 bg-red-100 border border-red-500 rounded-md my-4">
+      <h1 className="text-red-700 text-xl mb-2">Oops! Something went wrong</h1>
+      <p className="text-red-700">We encountered an error while processing your request.</p>
+      {/* <img src="path/to/error-image.png" alt="Error" className="max-w-full w-auto mt-2" /> */}
     </div>
   );
-};
-
-const styles = {
-  container: {
-    textAlign: "center",
-    padding: "20px",
-    backgroundColor: "#f8d7da", // Light red background color
-    border: "1px solid #dc3545", // Red border
-    borderRadius: "5px",
-    margin: "20px",
-  },
-  title: {
-    color: "#721c24", // Dark red text color
-  },
-  message: {
-    color: "#721c24", // Dark red text color
-  },
-  image: {
-    maxWidth: "100%",
-    height: "auto",
-    marginTop: "10px",
-  },
 };
 
 export default ErrorElement;
